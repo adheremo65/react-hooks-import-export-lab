4579f4ac3531bfe57a37b9cea559d69b02313f9d
Abdela umer <adheremo65@gmail.com>
2023-05-20 20:48:00 -0400

    react-hooks-import lab add

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import NavBar from "./NavBar"
+import Home from "./Home";
+import About from "./About";
 
-function App() {
+
+ function  App() {
   return (
     <div>
       <NavBar />
       <Home />
       <About />
     </div>
-  );
+  )
 }
-
 export default App;
+
